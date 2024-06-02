c898c41d4990b9e039b602d175bd74ca6f0b4e63
Sriharsha <harsha@lionsbot.com>
2024-06-02 21:09:29 +0800

    update: Modified run_ros2docker.sh script. removed docker files. Might add in future as per the requirement arises.

--- run_ros2docker.sh
+++ run_ros2docker.sh
@@ -1,8 +1,13 @@
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-export ROS_DEV_CONTAINER_NAME="ros2"
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+export ROS_DEV_CONTAINER_NAME="slam_vdb_ros_jazzy"
 export ROS_PROJECT_PATH="$SCRIPTPATH"
 
 echo "Creating ros2 docker container named $ROS_DEV_CONTAINER_NAME..."
-echo "Mounting the project directory $ROS_PROJECT_PATH to /home/ros2/ros2_ws/ in the container..."
+echo "Mounting the project directory $ROS_PROJECT_PATH to /slam_vdb_ws in the container..."
 
-docker-compose up -d --build
+docker pull ghanta1996/slam_vdb:ros_jazzy
+
+docker run -it -v $ROS_PROJECT_PATH:/slam_vdb_ws/slam_vdb -e DISPLAY=docker.for.mac.host.internal:0 ros:jazzy bash
