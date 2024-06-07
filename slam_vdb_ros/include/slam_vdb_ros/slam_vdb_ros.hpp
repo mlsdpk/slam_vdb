b619541f17b9f4f586f1199dc6e29b191db0bb6d
Sriharsha <harsha@lionsbot.com>
2024-06-08 00:48:57 +0800

    update: change header guard and added basic license for the file

--- slam_vdb_ros/include/slam_vdb_ros/slam_vdb_ros.hpp
+++ slam_vdb_ros/include/slam_vdb_ros/slam_vdb_ros.hpp
@@ -1,5 +1,25 @@
-#ifndef SLAM_VDB__SLAM_VDB_HPP_
-#define SLAM_VDB__SLAM_VDB_HPP_
+/**
+ * @file slam_vdb_ros.hpp
+ * @copyright Software License Agreement
+ *
+ * Copyright (c) 2024
+ *
+ * All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author
+ */
+#pragma once
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
@@ -25,5 +45,3 @@ private:
 };
 
 }  // namespace slam_vdb_ros
-
-#endif  // SLAM_VDB__SLAM_VDB_HPP_
